953f6d569e98e52555db084629206aa0cffdd1ba
Dillon Reed <c0038731@newcastle.ac.uk>
2024-03-02 11:05:17 +0000

    Updated naming/file structure for login/register pages to match naming scheme

--- app/src/main/java/com/example/learndigitalskills/ui/loginPage.java
+++ app/src/main/java/com/example/learndigitalskills/ui/loginPage.java
@@ -1,14 +1,16 @@
-package com.example.learndigitalskills;
+package com.example.learndigitalskills.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class register_page extends AppCompatActivity {
+import com.example.learndigitalskills.R;
+
+public class loginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register_page);
+        setContentView(R.layout.login_page);
     }
 }
--- app/src/main/java/com/example/learndigitalskills/ui/registerPage.java
+++ app/src/main/java/com/example/learndigitalskills/ui/registerPage.java
@@ -1,14 +1,16 @@
-package com.example.learndigitalskills;
+package com.example.learndigitalskills.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class login_page extends AppCompatActivity {
+import com.example.learndigitalskills.R;
+
+public class registerPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.register_page);
     }
 }
