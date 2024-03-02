5676f45ffdba1ac3db942466ccccfca5dd02c6e9
Dillon Reed <c0038731@newcastle.ac.uk>
2024-03-02 13:39:24 +0000

    Added placeholder navigation functionality to login button on loginPage to allow for testing of navigation view - will add authentication later

--- app/src/main/java/com/example/learndigitalskills/ui/loginPage.java
+++ app/src/main/java/com/example/learndigitalskills/ui/loginPage.java
@@ -5,15 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.learndigitalskills.R;
 
 public class loginPage extends AppCompatActivity {
 
+    Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        // Initialize buttons
+        loginButton = findViewById(R.id.login_button_login);
+
+        // Sets up listeners for button clicks
+        loginButton.setOnClickListener(v -> {
+            // Will add authentication later, just need to setup basic navigation for app
+            openBasePage();
+        });
+    }
+
+    private void openBasePage(){
+        Intent intent = basePage.getIntent(this);
+        startActivity(intent);
     }
 
     public static Intent getIntent(Context context){
