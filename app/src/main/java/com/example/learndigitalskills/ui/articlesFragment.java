8e697c54c22823ad2a1b81306d1522606ea02d20
Dillon Reed <c0038731@newcastle.ac.uk>
2024-03-11 18:42:03 +0000

    Implemented navigation functionality to filter dialogue

--- app/src/main/java/com/example/learndigitalskills/ui/articlesFragment.java
+++ app/src/main/java/com/example/learndigitalskills/ui/articlesFragment.java
@@ -1,12 +1,17 @@
 package com.example.learndigitalskills.ui;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.learndigitalskills.R;
 
@@ -16,6 +21,8 @@ import com.example.learndigitalskills.R;
  * create an instance of this fragment.
  */
 public class articlesFragment extends Fragment {
+
+    Button filterButton;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -62,5 +69,47 @@ public class articlesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.articles_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Bind UI elements
+        filterButton = view.findViewById(R.id.articles_button_filter);
+
+        // Setup listeners for buttons
+        filterButton.setOnClickListener(v -> {
+            // Create a layout inflator to use to create the dialogue box
+            LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+
+            // Inflate the layout file
+            View filterDialogueView = layoutInflater.inflate(R.layout.articles_filter_dialogue, null);
+
+            // Assigns the view to the dialogue
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+            builder.setView(filterDialogueView);
+            builder.setTitle("Filter");
+
+            // Setting up buttons for the dialogue
+            builder.setPositiveButton("Apply Filters", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+
+            builder.setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+            // Creates and shows the dialogue
+            AlertDialog articlesFilterDialogue = builder.create();
+            articlesFilterDialogue.show();
+        });
     }
 }
