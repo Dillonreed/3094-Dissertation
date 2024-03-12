b6472a61232f4386b2c341e5396321033dad28e9
Dillon Reed <c0038731@newcastle.ac.uk>
2024-03-12 14:55:57 +0000

    Added function necessary for navigation to an article page

---
+++ app/src/main/java/com/example/learndigitalskills/ui/articlePage.java
@@ -0,0 +1,22 @@
+package com.example.learndigitalskills.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.learndigitalskills.R;
+
+public class articlePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.article_page);
+    }
+
+    public static Intent getIntent(Context context){
+        return new Intent(context, articlePage.class);
+    }
+}
