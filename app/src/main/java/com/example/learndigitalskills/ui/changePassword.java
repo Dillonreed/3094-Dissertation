33e4a624e37541be505c421f234ea4d3539f93e7
Dillon Reed <c0038731@newcastle.ac.uk>
2024-03-13 09:39:07 +0000

    Added app action bar to change password page and code to customize and setup navigation functionality. Also added missing id for change password button.

--- app/src/main/java/com/example/learndigitalskills/ui/changePassword.java
+++ app/src/main/java/com/example/learndigitalskills/ui/changePassword.java
@@ -1,6 +1,8 @@
 package com.example.learndigitalskills.ui;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
@@ -10,13 +12,39 @@ import com.example.learndigitalskills.R;
 
 public class changePassword extends AppCompatActivity {
 
+    Toolbar changePasswordToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.change_password_page);
+
+        // Setup App Bar
+        changePasswordToolbar = findViewById(R.id.change_password_toolbar);
+        changePasswordToolbar.setTitleTextColor(getResources().getColor(R.color.white, null));
+        setSupportActionBar(changePasswordToolbar);
+
+        // Configure App Bar
+        ActionBar changePasswordActionBar = getSupportActionBar();
+        if (changePasswordActionBar != null){
+            // Setting title
+            changePasswordActionBar.setTitle(R.string.change_password_title_text);
+
+            // Customize back button
+            changePasswordActionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24);
+
+            // Enable back button
+            changePasswordActionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     public static Intent getIntent(Context context){
         return new Intent(context, changePassword.class);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
     }
 }
