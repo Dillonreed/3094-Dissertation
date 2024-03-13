62b4b10fc75245cb95595692194cb96574ca2899
Dillon Reed <c0038731@newcastle.ac.uk>
2024-03-13 10:11:56 +0000

    Added app action bar to help page and code to customize and setup navigation functionality.

--- app/src/main/java/com/example/learndigitalskills/ui/helpPage.java
+++ app/src/main/java/com/example/learndigitalskills/ui/helpPage.java
@@ -1,6 +1,8 @@
 package com.example.learndigitalskills.ui;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
@@ -10,13 +12,39 @@ import com.example.learndigitalskills.R;
 
 public class helpPage extends AppCompatActivity {
 
+    Toolbar helpToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help_page);
+
+        // Setup App Bar
+        helpToolbar = findViewById(R.id.help_toolbar);
+        helpToolbar.setTitleTextColor(getResources().getColor(R.color.white, null));
+        setSupportActionBar(helpToolbar);
+
+        // Configure App Bar
+        ActionBar helpActionBar = getSupportActionBar();
+        if (helpActionBar != null){
+            // Setting title
+            helpActionBar.setTitle(R.string.help_title_text);
+
+            // Customize back button
+            helpActionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24);
+
+            // Enable back button
+            helpActionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     public static Intent getIntent(Context context){
         return new Intent(context, helpPage.class);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
     }
 }
