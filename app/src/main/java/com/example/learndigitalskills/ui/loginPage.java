d35b14240931cff5bf89ecccd2a2e2e5e8b79f87
Dillon Reed <c0038731@newcastle.ac.uk>
2024-03-13 08:45:05 +0000

    Added app action bar to login page and code to customize and setup navigation functionality

--- app/src/main/java/com/example/learndigitalskills/ui/loginPage.java
+++ app/src/main/java/com/example/learndigitalskills/ui/loginPage.java
@@ -1,22 +1,43 @@
 package com.example.learndigitalskills.ui;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.learndigitalskills.R;
 
 public class loginPage extends AppCompatActivity {
 
+    Toolbar loginToolbar;
     Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        // Setup App Bar
+        loginToolbar = findViewById(R.id.my_toolbar);
+        loginToolbar.setTitleTextColor(getResources().getColor(R.color.white, null));
+        setSupportActionBar(loginToolbar);
+
+        // Configure App Bar
+        ActionBar loginActionBar = getSupportActionBar();
+        if (loginActionBar != null){
+            // Setting title
+            loginActionBar.setTitle(R.string.login_title);
+
+            // Customize back button
+            loginActionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24);
+
+            // Enable back button
+            loginActionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         // Initialize buttons
         loginButton = findViewById(R.id.login_button_login);
@@ -35,5 +56,11 @@ public class loginPage extends AppCompatActivity {
 
     public static Intent getIntent(Context context){
         return new Intent(context, loginPage.class);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
     }
 }
