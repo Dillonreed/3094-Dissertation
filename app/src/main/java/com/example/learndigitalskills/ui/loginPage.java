e65cdb683acb3240889890eea549a4ad7e635d15
Dillon Reed <c0038731@newcastle.ac.uk>
2024-04-10 20:31:24 +0100

    Added login functionality and checks to ensure username and password fields are filled. Also added flags to when base page is opened to not allow user to go back to the login page.

--- app/src/main/java/com/example/learndigitalskills/ui/loginPage.java
+++ app/src/main/java/com/example/learndigitalskills/ui/loginPage.java
@@ -1,25 +1,40 @@
 package com.example.learndigitalskills.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.learndigitalskills.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class loginPage extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
     Toolbar loginToolbar;
-    Button loginButton;
+    EditText editTextUsername, editTextPassword;
+    Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        // Initialize FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
 
         // Setup App Bar
         loginToolbar = findViewById(R.id.login_toolbar);
@@ -39,18 +54,63 @@ public class loginPage extends AppCompatActivity {
             loginActionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        // Initialize buttons
-        loginButton = findViewById(R.id.login_button_login);
+        // Bind UI elements
+        editTextUsername = findViewById(R.id.login_editText_username);
+        editTextPassword = findViewById(R.id.login_editText_password);
+        buttonLogin = findViewById(R.id.login_button_login);
 
         // Sets up listeners for button clicks
-        loginButton.setOnClickListener(v -> {
-            // Will add authentication later, just need to setup basic navigation for app
-            openBasePage();
+        buttonLogin.setOnClickListener(v -> {
+            if (checkFields()) {
+                loginUser();
+            }
         });
+    }
+
+    private boolean checkFields() {
+        // Ensure username field is filled
+        if (TextUtils.isEmpty(editTextUsername.getText().toString())) {
+            editTextUsername.setError("Field empty");
+            editTextUsername.requestFocus();
+            return false;
+        }
+
+        // Ensure password field is filled
+        if (TextUtils.isEmpty(editTextPassword.getText().toString())) {
+            editTextPassword.setError("Field empty");
+            editTextPassword.requestFocus();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void loginUser() {
+        String username = editTextUsername.getText().toString();
+        String password = editTextPassword.getText().toString();
+        String email = username + "@learndigitalskills.com";
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        // User logged in successfully
+                        Toast.makeText(loginPage.this, "User Logged In Successfully", Toast.LENGTH_SHORT).show();
+                        openBasePage();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // User failed to log in
+                        Toast.makeText(loginPage.this, "Log In Failed, Please Try Again", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     private void openBasePage(){
         Intent intent = basePage.getIntent(this);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 
