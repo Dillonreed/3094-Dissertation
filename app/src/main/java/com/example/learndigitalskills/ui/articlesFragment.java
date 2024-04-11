d53454e47b4465e072b184ea14e5a631188d0db0
Dillon Reed <c0038731@newcastle.ac.uk>
2024-04-11 07:25:44 +0100

    Added functionality to populate the list view on the articles fragment

--- app/src/main/java/com/example/learndigitalskills/ui/articlesFragment.java
+++ app/src/main/java/com/example/learndigitalskills/ui/articlesFragment.java
@@ -12,8 +12,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.learndigitalskills.R;
+import com.example.learndigitalskills.db.adapters.ArticlesListViewAdapter;
+import com.example.learndigitalskills.db.models.Article;
+import com.example.learndigitalskills.db.models.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +39,10 @@ import com.example.learndigitalskills.R;
  */
 public class articlesFragment extends Fragment {
 
-    Button filterButton;
+    private FirebaseUser currentUser;
+
+    Button buttonFilter;
+    ListView listViewArticles;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -75,13 +95,20 @@ public class articlesFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Initialize FirebaseUser
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
         // Bind UI elements
-        filterButton = view.findViewById(R.id.articles_button_filter);
+        buttonFilter = view.findViewById(R.id.articles_button_filter);
+        listViewArticles = view.findViewById(R.id.articles_listView_articles);
 
         // Setup listeners for buttons
-        filterButton.setOnClickListener(v -> {
+        buttonFilter.setOnClickListener(v -> {
             filterDialogue();
         });
+
+        // Populate ListView
+        populateListView();
     }
 
     private void filterDialogue() {
@@ -115,5 +142,61 @@ public class articlesFragment extends Fragment {
         // Creates and shows the dialogue
         AlertDialog articlesFilterDialogue = builder.create();
         articlesFilterDialogue.show();
+    }
+
+    private void populateListView() {
+        // Extract username from email, and capitalize the first letter
+        String username = currentUser.getEmail().substring(0, currentUser.getEmail().indexOf("@"));
+        username = username.substring(0, 1).toUpperCase() + username.substring(1);
+
+        // Retrieve user information from database
+        DocumentReference document = FirebaseFirestore.getInstance().document("users/" + username);
+        document.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        // Data retrieval successful
+
+
+                        // Retrieve articles from database
+                        CollectionReference articles = FirebaseFirestore.getInstance().collection("articles");
+
+                        articles.get()
+                                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                        // Data retrieval successful
+                                        // Map data to User object
+                                        User user = documentSnapshot.toObject(User.class);
+
+                                        // Map Articles to ArrayList of Articles
+                                        ArrayList<Article> articlesList = new ArrayList<>();
+
+                                        for (DocumentSnapshot document : queryDocumentSnapshots.getDocuments()) {
+                                            Article article = document.toObject(Article.class);
+                                            articlesList.add(article);
+                                        }
+
+                                        // Create and setup ListViewAdapter
+                                        ArticlesListViewAdapter adapter = new ArticlesListViewAdapter(getContext(), articlesList, user.getArticlesCompleted());
+                                        listViewArticles.setAdapter(adapter);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Data retrieval unsuccessful
+                                        Toast.makeText(getActivity(), "Data retrieval failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Data retrieval failed
+                        Toast.makeText(getActivity(), "Data retrieval failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
